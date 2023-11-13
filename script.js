f343de21a67be5591303a172f61fad50cf63b18c
PipouilleJohnson <killian.bouzera77150@gmail.com>
2023-11-13 11:13:41 +0100

    save 13112023 11h13

--- script.js
+++
@@ -1,5 +0,0 @@
-// Ajouter un écouteur d'événement pour le clic sur l'icône du menu
-document.querySelector('#menu-burger').addEventListener('click', function () {
-    // Inverser la classe "active" du menu pour afficher/cacher le menu
-    document.querySelector('#menu-burger').classList.toggle('active');
-});
